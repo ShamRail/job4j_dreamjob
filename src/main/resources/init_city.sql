aeaaf10bbbde42afa03bb443101015750beadc1f
ShamRail <rail1999g102r@mail.ru>
2022-11-22 17:59:11 +0500

    test deploy

--- src/main/resources/init_city.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO city(name) VALUES('Moscow');
-INSERT INTO city(name) VALUES('St. Petersburg');
-INSERT INTO city(name) VALUES('Ufa');
-INSERT INTO city(name) VALUES('Kazan');
-INSERT INTO city(name) VALUES('N. Novgorod');
-INSERT INTO city(name) VALUES('Volgograd');
-INSERT INTO city(name) VALUES('Krasnodar');
--- src/main/resources/init_user.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS _user(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(511),
-    email VARCHAR(255),
-    password VARCHAR(255)
-);
